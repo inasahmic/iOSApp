74bdca47699cc0af378a5231e39bd8e383b601df
Inas <inas_ahmic@yahoo.com>
2021-03-18 17:17:21 +0100

    update

--- iOSApp/LoginViewController.swift
+++ iOSApp/LoginViewController.swift
@@ -8,22 +8,39 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var password: CustomTxtField!
+    @IBOutlet weak var email: CustomTxtField!
+    
+    @IBAction func button(_ sender: Any) {
+        
+        //print ("Accepted !!")
+     let storyboard = UIStoryboard(name: "Registration", bundle: nil)
+     let viewcontoroller = storyboard.instantiateViewController(identifier: "RegistrationViewController")
+     self.present(viewcontoroller, animated: true, completion: {
+         //print("screen is presented")
+     })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
 
         // Do any additional setup after loading the view.
     }
     
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: self.email)
     }
-    */
+    
+    func isValidPassword() -> Bool {
+        let passwordRegex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*()\\-_=+{}|?>.<,:;~`’]{8,}$"
+        return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self.password)
+    }
+    
+
 
 }
--- iOSApp/RegistrationViewController.swift
+++ iOSApp/RegistrationViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
 
+    @IBOutlet weak var backToLogin: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        backToLogin.isUserInteractionEnabled = true
+        self.backToLogin.addGestureRecognizer(UIGestureRecognizer.init(target: self, action: #selector(self.tapBack(_:))))
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func tapBack (_ sender: UITapGestureRecognizer) {
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+        let viewcontoroller = storyboard.instantiateViewController(identifier: "LoginViewController")
+        self.present(viewcontoroller, animated: true, completion: {
+            //print("screen is presented")
+        })
     }
-    */
+    
 
 }
